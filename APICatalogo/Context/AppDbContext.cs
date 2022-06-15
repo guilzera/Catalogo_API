b1a621f43c5e9c7a3d35970baffb8122ba98f45b
guilzera <guiguidefarias@hotmail.com>
2022-06-15 13:25:58 -0300

    Entidades e configuracao do banco de dados.

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { }
+
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=NOTEBOOK-5KP7FMEG\\SQLEXPRESS;Database=APICATALOGO;Trusted_Connection=True;");
+    }
+}
