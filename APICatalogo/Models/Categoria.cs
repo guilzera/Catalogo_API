624f4f134078e2c80a1514b6f78e0d32864a2ff9
guilzera <guiguidefarias@hotmail.com>
2022-06-16 10:07:39 -0300

    Aplicando Data Annotations as propriedades das entidades

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,10 +1,20 @@
-﻿namespace APICatalogo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.Models;
 
 public class Categoria
 {
+    [Key]
     public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
-    public ICollection<Produto>? Produtos { get; set; } = new List<Produto>()
+
+    public ICollection<Produto>? Produtos { get; set; } = new List<Produto>();
 }
 
