9c814f3809e449e0ada2487166543eab26085183
guilzera <guiguidefarias@hotmail.com>
2022-06-17 14:02:01 -0300

    Metodo Post criado

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -23,12 +23,22 @@ namespace APICatalogo.Controllers
             return produtos;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name="ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-            if (produto == null) return NotFound("Id não identificado...");
+            if (produto is null) return NotFound("Produto não encontrado...");
             return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null) return BadRequest();
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterProduto", 
+                new { id = produto.ProdutoId }, produto);
         }
     }
 }
