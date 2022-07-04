47c6880387a119a438c60b787ced4a8a09816c4f
Guilherme Nascimento <guiguidefarias@hotmail.com>
2022-07-04 11:52:42 -0300

    Visual do código aprimorado

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -1,12 +1,11 @@
 ﻿using APICatalogo.Context;
 using APICatalogo.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controllers
 {
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class CategoriasController : ControllerBase
     {
@@ -29,16 +28,19 @@ namespace APICatalogo.Controllers
         {
             try
             {
-                throw new DataMisalignedException();
                 var categoria = _context.Categorias?.AsNoTracking().Take(10).ToList();
-                if (categoria is null) return NotFound("categoria não encontrada...");
+                if (categoria is null)
+                {
+                    return NotFound("categoria não encontrada...");
+                }
+
                 return categoria;
             }
             catch (Exception)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError,
                     "Ocorreu um erro ao tratar sua solicitação");
-            } 
+            }
         }
 
         [HttpGet("{id:int}", Name = "ObterCategoria")]
@@ -47,7 +49,11 @@ namespace APICatalogo.Controllers
             try
             {
                 var categoria = _context.Categorias?.AsNoTracking().FirstOrDefault(p => p.CategoriaId == id);
-                if (categoria is null) return NotFound($"Categoria com o id{id} não localizada");
+                if (categoria is null)
+                { 
+                    return NotFound($"Categoria com o id{id} não localizada");
+                }
+
                 return Ok(categoria);
             }
             catch (Exception)
@@ -60,9 +66,14 @@ namespace APICatalogo.Controllers
         [HttpPost]
         public ActionResult Post(Categoria categoria)
         {
-            if (categoria is null) return BadRequest("Dados Inválidos");
+            if (categoria is null)
+            {
+                return BadRequest("Dados Inválidos");
+            }
+
             _context.Categorias.Add(categoria);
             _context.SaveChanges();
+
             return new CreatedAtRouteResult("ObterCategoria",
                 new { id = categoria.CategoriaId }, categoria);
         }
@@ -70,9 +81,14 @@ namespace APICatalogo.Controllers
         [HttpPut("{id:int}")]
         public ActionResult Put(int id, Categoria categoria)
         {
-            if (id != categoria.CategoriaId) return BadRequest("Dados Inválidos");
+            if (id != categoria.CategoriaId)
+            {
+                return BadRequest("Dados Inválidos");
+            }
+
             _context.Entry(categoria).State = EntityState.Modified;
             _context.SaveChanges();
+
             return Ok(categoria);
         }
 
@@ -80,9 +96,14 @@ namespace APICatalogo.Controllers
         public ActionResult Delete(int id)
         {
             var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
-            if (categoria is null) return NotFound($"categoria com o id{id} não localizada");
+            if (categoria is null)
+            {
+                return NotFound($"categoria com o id{id} não localizada");
+            }
+
             _context.Categorias.Remove(categoria);
             _context.SaveChanges();
+
             return Ok(categoria);
         }
     }
--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controllers
 {
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class ProdutosController : ControllerBase
     {
@@ -22,7 +22,11 @@ namespace APICatalogo.Controllers
             try
             {
                 var produtos = _context.Produtos?.AsNoTracking().Take(10).ToList();
-                if (produtos is null) return NotFound("Produtos não encontrados...");
+                if (produtos is null)
+                {
+                    return NotFound();
+                }
+
                 return produtos;
             }
             catch (Exception)
@@ -32,13 +36,16 @@ namespace APICatalogo.Controllers
             }
         }
 
-        [HttpGet("{id:int}", Name="ObterProduto")]
+        [HttpGet("{id:int:min(1)}", Name = "ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             try
             {
                 var produto = _context.Produtos?.AsNoTracking().FirstOrDefault(p => p.ProdutoId == id);
-                if (produto is null) return NotFound($"Produto com o id{id} não localizada");
+                if (produto is null)
+                {
+                    return NotFound();
+                }
                 return produto;
             }
             catch (Exception)
@@ -51,19 +58,29 @@ namespace APICatalogo.Controllers
         [HttpPost]
         public ActionResult Post(Produto produto)
         {
-            if (produto is null) return BadRequest("Dados Inválidos");
+            if (produto is null)
+            {
+                return BadRequest();
+            }
+
             _context.Produtos.Add(produto);
             _context.SaveChanges();
-            return new CreatedAtRouteResult("ObterProduto", 
+
+            return new CreatedAtRouteResult("ObterProduto",
                 new { id = produto.ProdutoId }, produto);
         }
 
         [HttpPut("{id:int}")]
         public ActionResult Put(int id, Produto produto)
         {
-            if (id != produto.ProdutoId) return BadRequest("Dados Inválidos");
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest("Dados Inválidos");
+            }
+
             _context.Entry(produto).State = EntityState.Modified;
             _context.SaveChanges();
+
             return Ok(produto);
         }
 
@@ -71,9 +88,15 @@ namespace APICatalogo.Controllers
         public ActionResult Delete(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-            if (produto is null) return NotFound($"Produto com o id{id} não localizada");
+
+            if (produto is null)
+            {
+                return NotFound($"Produto com o id{id} não localizada");
+            }
+
             _context.Produtos.Remove(produto);
             _context.SaveChanges();
+
             return Ok(produto);
         }
     }
